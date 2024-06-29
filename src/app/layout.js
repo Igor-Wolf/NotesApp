51a8fedd6df6b15d38373cf7b5f6160d04ab5fe0
Igor <igorreisbarbosa@gmail.com>
2024-06-28 23:12:51 -0300

    development

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>  
+        
+      </head>
       <body className={inter.className}>
         {children}
       </body>
